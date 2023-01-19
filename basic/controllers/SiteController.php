89ee14df4f0d99e68f5d9a039c156c931d24bd3e
kateryna_pohorila <kateryna.pohorila@advantiss.com>
2023-01-20 00:39:41 +0200

    add comments

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -12,7 +12,8 @@ use app\models\ContactForm;
 use app\models\Article;
 use yii\data\Pagination;
 use app\models\Topic;
-
+use app\models\Comment;
+use app\models\CommentForm;
 
 class SiteController extends Controller
 {
@@ -131,11 +132,25 @@ class SiteController extends Controller
         $recent = Article::find()->orderBy('date desc')->limit(3)->all();
         $topics = Topic::find()->all();
 
+        $comments = $article->comments;
+        $commentsParent = array_filter($comments, function ($k) {
+            return $k['comment_id'] == null;
+        });
+
+        $commentsChild = array_filter($comments, function ($k) {
+            return ($k['comment_id'] != null && !$k['delete']);
+        });
+
+        $commentForm = new CommentForm();
+
         return $this->render('single', [
             'article' => $article,
             'popular' => $popular,
             'recent' => $recent,
             'topics' => $topics,
+            'commentsParent' => $commentsParent,
+            'commentsChild' => $commentsChild,
+            'commentForm' => $commentForm,
         ]);
     }
 
@@ -161,5 +176,28 @@ class SiteController extends Controller
             'recent' => $recent,
             'topics' => $topics,
         ]);
+    }
+
+    public function actionComment($id, $id_comment = null)
+    {
+        $model = new CommentForm();
+        if (Yii::$app->request->isPost) {
+            $model->load(Yii::$app->request->post());
+            if ($model->saveComment($id, $id_comment)) {
+                return $this->redirect(['site/view', 'id' => $id]);
+            }
+        }
+    }
+
+    public function actionCommentDelete($id, $id_comment)
+    {
+        if (Yii::$app->request->isPost) {
+            $data = Comment::findOne($id_comment);
+            if ($data->user_id == Yii::$app->user->id) {
+                $data->delete = true;
+                $data->save(false);
+            }
+            return $this->redirect(['site/view', 'id' => $id]);
+        }
     }
 }
--- basic/models/User.php
+++ basic/models/User.php
@@ -159,4 +159,9 @@ class User extends \yii\db\ActiveRecord implements IdentityInterface
     {
         return $this->save(false);
     }
+
+    public function getDate()
+    {
+        return Yii::$app->formatter->asDate($this->date);
+    }
 }
